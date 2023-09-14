146dd35d55ff1677d87ea070fd21f0e6249bd65b
gabrielcbm <116837225+gabrielcbm@users.noreply.github.com>
2023-09-14 16:24:39 -0300

    Divisão de Códigos
    Separei os códigos das atividades

--- JavaScript&&OO/JS/ativ8Notas1.js
+++ JavaScript&&OO/JS/ativ8Notas1.js
@@ -20,25 +20,3 @@ function calcularMedia (media){
     var result = (media)/qtdNotas;
     return alert(`Média = ${result}`);
 }
-
-//Segundo Código
-//Criação de variaveis
-var notas = 0;
-var qtdNotas = parseInt(prompt("Digite a quantidade de notas que deseja inserir: "));
-var nota;
-var mediaAprovacao = 7;
-
-//criação de sistema de repetição para o usuário inserir as notas
-for (let index = 0; index < qtdNotas ; index++) {
-    nota = (parseInt(prompt(`Digite a ${index + 1}ª nota: `)));
-    notas += nota;    
-}
-
-//chamando a funçao e imprimindo
-calcularNota(notas);
-
-//criação de função
-function calcularNota(not) {
-    var resultado = (mediaAprovacao*(qtdNotas+1))- not;
-    return alert(`Nota Necessária para passar = ${resultado}`);
-}
